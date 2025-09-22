a508e8c5212c558b18fc2106c81dfc2900288133
Genius-Excel <geniusexcel9@gmail.com>
2025-09-22 10:54:46 +0100

    Remove unused Vonage SMS sender function and related imports from utils.py

--- base_app/utils.py
+++ base_app/utils.py
@@ -1,7 +1,6 @@
 import mailtrap as mt
 from django.conf import settings
 from decouple import config 
-import vonage
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 
@@ -17,29 +16,6 @@ def custom_email_sender(email_address, subject, message, sender_name):
 
     client = mt.MailtrapClient(token=settings.SMTP_API_TOKEN)
     client.send(mail)
-
-
-
-VONAGE_API_KEY=config("VONAGE_API_KEY")
-VONAGE_API_SECRET=config("VONAGE_API_SECRET")
-
-def custom_sms_sender(sms_sender, sms_recipient, sms_message):
-    client = vonage.Client(key=VONAGE_API_KEY, secret=VONAGE_API_SECRET)
-
-    print("Sending SMS")
-    try:
-        response_data = client.sms.send_message({
-            "from": sms_sender,
-            "to": sms_recipient,
-            "text": sms_message,
-        })
-
-        print("SMS Sent success")
-        print(response_data)
-    except Exception as e:
-        print(f"Not sending because: {e}")
-
-
 
 # Email sender with HTML template format
 
